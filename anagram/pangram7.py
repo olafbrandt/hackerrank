12cc6eb9431d5120a93cc58d209b653406add879
Olaf Brandt <olaf.brandt@gmail.com>
2018-11-16 16:55:50 -0800

    olaf -  pretty print the pangram solutions.

--- anagram/pangram7.py
+++ anagram/pangram7.py
@@ -13,25 +13,39 @@ content = [x.strip() for x in content]
 
 # define the hash function
 def hashu(w):
-	return (''.join(sorted(set(w))))
+    return (''.join(sorted(set(w))))
 
 def hash(w):
-	return (''.join(sorted(list(w))))
+    return (''.join(sorted(list(w))))
 
 # build the pangram solving dicionary
 pangram_dict = defaultdict(list)
 for w in content:
-	h = hashu(w)
+	h = hashu(w)	
 	if len(w) >= 4:
 		pangram_dict[h].append(w)
+
+def pp_word_list(wl):
+	max_len = max([len(x) for x in wl])
+	wpl = (80-4) // (max_len+2)
+	i = 0
+	while (i < len(wl)):
+		print('    ', end='')
+		for j in range(wpl):
+			if ((i+j) < len(wl)):
+				print ('{:{w}s}  '.format(wl[i+j], w=max_len), end='')
+		i += wpl
+		print('')
 
 #run the solving service
 try:
 	while True:
-		w = input('Pangram: ')
+		w = input('Pangram: ').strip()
 		if len(w) < 1: break
 		h = hashu(w)
-		print ('{} --> {}'.format(w, ','.join(h)))
+		hset = set(h)
+		hset.remove(w[0])
+		print ('{} --> [{}],{}'.format(w, w[0], ','.join(hset)))
 		pangrams = []
 		subpans = []
 		for e in pangram_dict.items():
@@ -40,8 +54,11 @@ try:
 					pangrams.extend(e[1])
 				else:
 					subpans.extend(e[1])
-		print('Pangrams: {}'.format(sorted(pangrams)))
-		print('Others:   {}'.format(sorted(subpans)))
+
+		print('Pangrams:')
+		pp_word_list(sorted(pangrams))
+		print('Others:')
+		pp_word_list(sorted(subpans))
 except EOFError:
 	pass
 
